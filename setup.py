f6dd3cb5f65a902919c59b509e32cb7f83acf963
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2023-02-14 22:41:43 -0300

    chore: bump  v0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [
 
 setuptools.setup(
     name='sunweg',
-    version='0.0.1',
+    version='0.0.3',
     author="rokam",
     author_email="lucas@mindello.com.br",
     description="A library to retrieve data from sunweg.net",
