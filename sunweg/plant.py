a2e79a0b5f382adda74c224114ed480b9d3c7295
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2023-02-24 12:17:17 -0300

    chore(plant): typing

--- sunweg/plant.py
+++ sunweg/plant.py
@@ -1,10 +1,24 @@
 from datetime import datetime
 
+from .device import Inverter
+
+
 class Plant:
-    inverters = []
-    
-    def __init__(self, id:int, name:str, total_power:float, kwh_per_kwp:float, performance_rate:float, saving:float,
-                    today_energy:float, total_energy:float, total_carbon_saving:float, last_update:datetime):
+    inverters: list[Inverter] = []
+
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        total_power: float,
+        kwh_per_kwp: float,
+        performance_rate: float,
+        saving: float,
+        today_energy: float,
+        total_energy: float,
+        total_carbon_saving: float,
+        last_update: datetime,
+    ):
         self.id = id
         self.name = name
         self.total_power = total_power
@@ -15,6 +29,6 @@ class Plant:
         self.total_energy = total_energy
         self.total_carbon_saving = total_carbon_saving
         self.last_update = last_update
-    
+
     def __str__(self) -> str:
         return str(self.__class__) + ": " + str(self.__dict__)
