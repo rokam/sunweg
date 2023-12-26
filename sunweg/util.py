5dd93bd76e3ed2a2acd04a0ae2f4ff048b56f60b
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2023-12-26 12:13:43 -0300

    feat(statistics): initial support

--- sunweg/util.py
+++ sunweg/util.py
@@ -1,4 +1,5 @@
 """Sunweg API util."""
+from datetime import date
 from enum import Enum
 
 
@@ -8,3 +9,38 @@ class Status(Enum):
     OK = 0
     WARN = 2
     ERROR = 1
+
+class ProductionStats:
+    """Energy production statistics"""
+    def __init__(self, date:date, production:float, prognostic:float) -> None:
+        """
+        Initialize energy production statistics.
+        
+        :param date: statistics date
+        :type date: date
+        :param production: statistics production in kWh
+        :type production: float
+        :param prognostic: statistics expected production in kWh
+        """
+        self._date = date
+        self._production = production
+        self._prognostic = prognostic
+
+    @property
+    def date(self) -> date:
+        """Get date."""
+        return self._date
+
+    @property
+    def production(self) -> float:
+        """Get energy production in kWh."""
+        return self._production
+    
+    @property
+    def prognostic(self) -> float:
+        """Get expected energy production in kWh."""
+        return self._prognostic
+    
+    def __str__(self) -> str:
+        """Cast Phase to str."""
+        return str(self.__class__) + ": " + str(self.__dict__)
